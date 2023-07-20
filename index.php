b95449b5a21e2fd478eeb5bee0181a2247083460
Rabsha Shakeel <52667020+Rabsha@users.noreply.github.com>
2023-07-20 10:14:18 +0500

    First Push

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Care Plan Report</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="formwork">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12"></div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="myforms">
+                        <h1>Personal Care Plan</h1>
+                        <form>
+                            <div class="form-group">
+                                <label>Client Name</label>
+                                <input type="text" name="ClientName" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Ages</label>
+                                <input type="text" name="ClientAge" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>About Me</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>What are the Client's Desired Goals and Outcomes?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Does the Client have Cognitive Impairment?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>What are the Client's Personal Care Needs?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Does the Client have Continence Care Needs ?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>What are the Client's Mobility Care Needs ?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>What are the Client's Meal Requirements ?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>What are the Client's Medication Support Requirements ?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Client Advance Support Requirements ?</label>
+                                <textarea name="aboutme" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button>Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12"></div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
